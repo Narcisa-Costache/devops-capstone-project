56deba30354a42ecac487b846c59250d7fb775ff
Narcisa-Costache <narcisa.bucioaca@gmail.com>
2025-03-05 07:24:08 -0500

    Update Account implementation

--- service/routes.py
+++ service/routes.py
@@ -85,9 +85,22 @@ def read_account(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_account(account_id):
+    """
+    Update an Account
+    This endpoint will update an Account based the account_id that is requested
+    """
+    app.logger.info("Request to update an Account with id: %s", account_id)
+    account = Account.find(account_id)
 
-# ... place you code here to UPDATE an account ...
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
+    account.deserialize(request.get_json())
+    account.update()
+
+    return jsonify(account.serialize()), status.HTTP_200_OK
 
 ######################################################################
 # DELETE AN ACCOUNT
