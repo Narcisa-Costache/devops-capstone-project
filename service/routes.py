3351f41e2d2bc4efab9c697e4d3c2ef8065f7968
Narcisa-Costache <narcisa.bucioaca@gmail.com>
2025-03-05 07:48:38 -0500

    List Account Implementation

--- service/routes.py
+++ service/routes.py
@@ -60,9 +60,18 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
-# ... place you code here to LIST accounts ...
-
+@app.route("/accounts", methods=["GET"])
+def list_accounts():
+    """
+    List All Accounts
+    This endpoint will List All Accounts
+    """
+    app.logger.info("Request to List All Accounts")
+    accounts = Account.all()
+    
+    account_list = [account.serialize() for account in accounts]
+    app.logger.info("Returning [%s] accounts", len(account_list))
+    return jsonify(account_list), status.HTTP_200_OK
 
 ######################################################################
 # READ AN ACCOUNT
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -176,3 +176,16 @@ class TestAccountService(TestCase):
         account_id = 0
         resp = self.client.put(f"{BASE_URL}/{account_id}", content_type="application/json")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_list_accounts(self):
+        """It should List All Accounts"""
+        self._create_accounts(5)
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
+
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
